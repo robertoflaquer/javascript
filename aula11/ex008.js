4f11ce7c525629bca81ae02388603b204b515207
Roberto Flaquer Moura <betoflaquer@gmail.com>
2022-06-17 10:52:30 -0300

    ex 8,9 e 10

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel} por hora.`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Foi multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
