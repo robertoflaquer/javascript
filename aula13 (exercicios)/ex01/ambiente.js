46ca2b43f160e3de0bb5d7e920e4b3f829bbbf4d
Roberto Flaquer Moura <betoflaquer@gmail.com>
2022-11-02 21:21:55 -0300

    att

---
+++ aula13 (exercicios)/ex01/ambiente.js
@@ -0,0 +1,24 @@
+var contador = 1
+
+while (contador <= 6) {
+    console.log(`Tudo bem? pela ${contador} vez. `)
+    contador++
+}
+
+do {
+    console.log(`Tudo bem? pela ${contador} vez. `)
+    contador++
+} while (contador <=6)
+
+
+
+
+
+/*
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
